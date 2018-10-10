45ff4d5d6f60aa15c1e26eeca2f2e0817f1d4581
Niels Groeneveld <j.c.groeneveld@students.uu.nl>
2018-10-10 15:01:58 +0200

    Updated GameState, added Board and BoardField

---
+++ Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace reversi
+{
+    class Board
+    {
+        // Create a 2d array of BoardFields
+
+        /**
+         * Should also:
+         * Keep track of last played move
+         * Keep track of recently changed stones
+         */
+
+        public Board()
+        {
+
+        }
+    }
+}
--- GameState.cs
+++ GameState.cs
@@ -14,6 +14,10 @@ namespace reversi
         const String TIE        = "state tie";
 
 
+        Board board;
+        String state;
+        int blueStones, redStones;
+
         public GameState()
         {
 
